d01311c9876605044139dfccfe006f8cc9f1b53d
mikaylamcgraw <mikayla.mcgraw@sage.com>
2021-09-17 09:51:32 -0700

    Adding inital UI work

---
+++ CapstoneMusic.App/CapstoneMusic.Shared/Playlist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapstoneMusic.Shared
+{
+   public class Playlist
+    {
+        public string Name { get; set; }
+        public string User { get; set; }
+        public int SongCount { get; set; }
+        public int PlaylistDuration { get; set; }
+        public enum Type
+        {
+            Personal,
+            Shared
+        }
+    }
+}
