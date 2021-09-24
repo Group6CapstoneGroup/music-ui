86f3bd5c38cf3fdeebf06e9ba0b706839b00c4b9
mikaylamcgraw <mikayla.mcgraw@sage.com>
2021-09-23 20:21:52 -0700

    Commiting change to project

---
+++ MusicRecommendation.App/Components/Login.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicRecommendation.App.Components
+{
+    public partial class Login
+    {
+        protected string UserName { get; set; }
+        protected string Password { get; set; }
+    }
+}
