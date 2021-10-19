e40eb803b410b950fa904b7eab57a1b3d5abddfa
mikaylamcgraw <mikayla.mcgraw@sage.com>
2021-10-19 16:51:33 -0700

    Adding work to DataGrid

---
+++ MusicRecommendation.App/Models/Music.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicRecommendation.App.Models
+{
+    public class Music
+    {
+        public string Song { get; set; }
+
+        public string Artist { get; set; }
+
+        public string Album { get; set; }
+    }
+}
