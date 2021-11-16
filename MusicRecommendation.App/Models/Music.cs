cc2d0c6980ac0f8600420de5e3d3aa1391f78da5
mikaylarmcgraw <mikaylamcgraw@yahoo.com>
2021-11-16 12:17:01 -0800

    Adding JSON property serializer header on Music model

--- MusicRecommendation.App/Models/Music.cs
+++ MusicRecommendation.App/Models/Music.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace MusicRecommendation.App.Models
@@ -10,9 +11,13 @@ namespace MusicRecommendation.App.Models
     {
         public Guid RecordId { get; set; }
         public long RecordNumber { get; set; }
+        [JsonPropertyName("track")]
         public string Track { get; set; }
+        [JsonPropertyName("artist")]
         public string Artist { get; set; }
+        [JsonPropertyName("album")]
         public string Album { get; set; }
+        [JsonPropertyName("playlist")]
         public string Playlist { get; set; }
     }
 }
