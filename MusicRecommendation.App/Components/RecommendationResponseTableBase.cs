8529b69d05acbcadc3c3dc3e31b39b8e0b0898d2
mikaylarmcgraw <mikaylamcgraw@yahoo.com>
2021-11-26 21:51:04 -0800

    Adding UI recommendation response component

---
+++ MusicRecommendation.App/Components/RecommendationResponseTableBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using MusicRecommendation.App.Models;
+using MusicRecommendation.App.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicRecommendation.App.Components
+{
+    public class RecommendationResponseTableBase : ComponentBase
+    {
+        [Inject]
+        public IMusicService MusicService { get; set; }
+        public IEnumerable<Music> songList { get; set; }
+    }
+}
