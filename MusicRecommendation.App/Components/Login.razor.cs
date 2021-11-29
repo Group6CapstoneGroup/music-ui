4680e3297f4c30dafaac47fa2320050350de59db
mikaylarmcgraw <mikaylamcgraw@yahoo.com>
2021-11-29 12:26:18 -0800

    Adding minor authentication

--- MusicRecommendation.App/Components/Login.razor.cs
+++ MusicRecommendation.App/Components/Login.razor.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 
 namespace MusicRecommendation.App.Components
 {
     public partial class Login
     {
         protected string UserName { get; set; }
+
         protected string Password { get; set; }
     }
 }
