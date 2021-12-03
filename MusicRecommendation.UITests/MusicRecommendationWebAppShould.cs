1d2be898c476492ac0da8841861a57b88560e8c6
mikaylarmcgraw <mikaylamcgraw@yahoo.com>
2021-12-03 13:16:27 -0800

    Adding UI tests and fixing loader issues

---
+++ MusicRecommendation.UITests/MusicRecommendationWebAppShould.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Xunit;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace MusicRecommendation.UITests
+{
+    public class MusicRecommendationWebAppShould
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationHomePage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:3000/");
+
+                string pageTitle = driver.Title;
+
+                Assert.Equal("Tune Tip - Home Page", pageTitle);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationLoginPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:3000/");
+
+                string pageTitle = driver.Title;
+
+                Assert.Equal("Tune Tip - Home Page", pageTitle);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationAccountPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:3000/");
+
+                string pageTitle = driver.Title;
+
+                Assert.Equal("Tune Tip - Home Page", pageTitle);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationSupportPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:3000/");
+
+                string pageTitle = driver.Title;
+
+                Assert.Equal("Tune Tip - Home Page", pageTitle);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationAboutPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:3000/");
+
+                string pageTitle = driver.Title;
+
+                Assert.Equal("Tune Tip - Home Page", pageTitle);
+            }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void LoadApplicationRecommendationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:3000/");
+
+                string pageTitle = driver.Title;
+
+                Assert.Equal("Tune Tip - Home Page", pageTitle);
+            }
+        }
+    }
+}
